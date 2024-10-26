8beb53640631460e5356370f8e3821683ba16722
Tran Anh Vu <tav04092004@gmail.com>
2024-10-26 13:08:08 +0700

    I think this file is error

--- client/collabnote/src/contexts/editor-context.tsx
+++ client/collabnote/src/contexts/editor-context.tsx
@@ -177,7 +177,8 @@ export const EditorProvider = ({children}: EditorProviderInterface) => {
         setCurrentFont,
         focusEditor,
         handleEditorChange,
-      }}>
+      }}
+    >
       {children}
     </EditorContext.Provider>
   );
